a8040005580d72623246138d4834467e38b52d6e
Joshua Ross <josh@rossjm.dev>
2024-07-22 22:53:35 -0400

    added line break

--- python-fundamentals/working-with-files/acronyms.py
+++ python-fundamentals/working-with-files/acronyms.py
@@ -20,7 +20,7 @@ def add_acronym():
     
 def main():
     # Ask the user whether they want to find or add an acronym
-    choice = input('Do you want to find(F) or add(A) an acronym?')
+    choice = input('Do you want to find(F) or add(A) an acronym?\n')
     if choice == 'F':
         find_acronym()
     elif choice == 'A':
